ecc1d9c5fa14c92d9588aee18aa40b08774a40d7
Rogoberto Ribeiro <usbetin@gmail.com>
2025-06-05 23:16:00 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,27 @@
-const CACHE_NAME = "borderotech-cache-v1"; //modificar quando atualizar
+const CACHE_NAME = "borderotech-cache-v1";
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/login.html",
-  "/mensagem.html",
-  "/assets/tailwind.min.css",
-  "/assets/fonts/fonts.css",
-  "/assets/favicon.png",
-  "/assets/favicon.ico",
-  // coloque aqui todos os assets que quer cachear
+  "./",
+  "./index.html",
+  "./login.html",
+  "./mensagem.html",
+  "./assets/tailwind.min.css",
+  "./assets/fonts/fonts.css",
+  "./assets/manifest.json",
+  "./assets/favicon.png",
+  "./assets/favicon.ico"
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
+    caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
+    caches.match(event.request).then((response) => {
       return response || fetch(event.request);
     })
   );
